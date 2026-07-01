78b2ccde833118de0bac068028ed17fb8f33919b
moslem <uddinemuslim@gmail.com>
2026-07-01 22:55:34 +0530

    Todo Practice

---
+++ practiceReact/todoPractice/src/context/TodoContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+
+    todos: [
+        {
+            id: 1,
+            todo: "todoMsg",
+            completed: false
+        }
+    ],
+    theme: "dark",
+    addTodo: (todo) => {},
+    updateTodo: ( id, todo ) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.provider
