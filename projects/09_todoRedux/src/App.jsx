1f678110cb1672bf67659b3b222896bd2c6ebc13
moslem <uddinemuslim@gmail.com>
2026-07-03 23:16:54 +0530

    Creating todo using redux

---
+++ projects/09_todoRedux/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-3xl text-center bg-gray-900 text-white rounded-2xl '> Learn Redux toolkit with react </h1>
+    </>
+  )
+}
+
+export default App
