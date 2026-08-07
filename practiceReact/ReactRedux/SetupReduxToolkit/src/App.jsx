3834c11f03f0a1150c3723ea4616e3f24e6b6018
uddin <uddin@iisi.iitg.ac.in>
2026-08-07 16:03:50 +0530

    Initialize Redux Toolkit setup

---
+++ practiceReact/ReactRedux/SetupReduxToolkit/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-center'>Welcome to Redux toolkit Review</h1>
+      <p className="text-xl text-center">In this project I am trackign and learnign react redux</p>
+    </>
+  )
+}
+
+export default App
