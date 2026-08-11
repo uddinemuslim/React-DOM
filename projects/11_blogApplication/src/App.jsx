607d5b0129fa110bd1bce4af01a94c46a7749d36
uddin <uddin@iisi.iitg.ac.in>
2026-08-11 15:17:36 +0530

    Added ENV configuration files for the blog app

---
+++ projects/11_blogApplication/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1 className='text-3xl text-center mt-2.5 text-amber-50 pb-3 pt-2 mb-2.5 bg-gray-600'>Blog Apps</h1>
+    </>
+  )
+}
+
+export default App
