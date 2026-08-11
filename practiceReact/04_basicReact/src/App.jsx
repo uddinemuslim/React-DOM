e28ff0bd5e8249a90e8ef5ba9208c15a343cfd41
uddin <uddin@iisi.iitg.ac.in>
2026-08-11 17:55:01 +0530

    Refactor App component to conditionally render ParrotCard or heading

--- practiceReact/04_basicReact/src/App.jsx
+++ practiceReact/04_basicReact/src/App.jsx
@@ -1,11 +1,18 @@
 import ParrotCard from "./components/parrotCard"
 
 function App() {
+  const condition = {
+    parrotCard: true,
+    heading: false
+  }
 
   return (
     <>
-    <h1 className='text-3xl  text-center bg-amber-100'>Blogs</h1>
-    <ParrotCard/>
+      {!condition.parrotCard ? (
+        <ParrotCard />
+      ) : (
+        <h1 className='text-3xl text-center bg-amber-100'>Blogs</h1>
+      )}
     </>
   )
 }
